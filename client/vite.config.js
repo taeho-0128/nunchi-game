c5bd7f42f9c323137923b8e48ee4b2350ba471f5
taeho <jeff0128@naver.com>
2025-04-15 23:31:45 +0900

    🚑 fix: add base path for Vercel routing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 👈 추가
   server: {
     port: 5173,
     proxy: {
       '/socket.io': {
         target: 'http://localhost:3000',
-        ws: true
+        ws: true,
       }
     }
   }
